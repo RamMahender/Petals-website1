a6a5009de0cbf0514c51fd4f4c04baecff72f034
Ram Mahender <ram.mahender@gmail.com>
2019-10-02 12:10:25 +0530

    First commit
    First commit

---
+++ precache-manifest.f51be2671e04755e4e0cb12c823c19ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a6e53fce12d63510c7f3be297aef9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccfc0190319c1e389dac",
+    "url": "/static/css/2.79141403.chunk.css"
+  },
+  {
+    "revision": "43c79f47ec945fb97570",
+    "url": "/static/css/main.ab08b4fb.chunk.css"
+  },
+  {
+    "revision": "ccfc0190319c1e389dac",
+    "url": "/static/js/2.c925d590.chunk.js"
+  },
+  {
+    "revision": "43c79f47ec945fb97570",
+    "url": "/static/js/main.a8cad35b.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "9bc77c3bca968c7490de95d1532d0e87",
+    "url": "/static/media/Montserrat-ExtraBold.9bc77c3b.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/static/media/Montserrat-Regular.9c460951.ttf"
+  }
+]);
