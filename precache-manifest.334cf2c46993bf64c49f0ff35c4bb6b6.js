f4c5519330cfa7b7c7e41e8f8262e19f9a6bc59a
Ram Mahender <ram.mahender@gmail.com>
2019-10-21 12:53:45 +0530

    changes

---
+++ precache-manifest.334cf2c46993bf64c49f0ff35c4bb6b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af78e919bfe3518c56cd5fff7192cd60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1af7d885babc160a1cf",
+    "url": "/static/css/2.79141403.chunk.css"
+  },
+  {
+    "revision": "cac2b450aaf3733cd816",
+    "url": "/static/css/main.75d2cd39.chunk.css"
+  },
+  {
+    "revision": "c1af7d885babc160a1cf",
+    "url": "/static/js/2.d437427e.chunk.js"
+  },
+  {
+    "revision": "cac2b450aaf3733cd816",
+    "url": "/static/js/main.bdfc67d8.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "9bc77c3bca968c7490de95d1532d0e87",
+    "url": "/static/media/Montserrat-ExtraBold.9bc77c3b.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/static/media/Montserrat-Regular.9c460951.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c3a71e3fe16251ff4631a77147c97d0.js"
+  "/precache-manifest.334cf2c46993bf64c49f0ff35c4bb6b6.js"
 );
 
 self.addEventListener('message', (event) => {
